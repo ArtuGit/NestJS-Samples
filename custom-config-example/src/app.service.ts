03b0213ab8a9d1d0f2defc3234ea968671afcd53
Artem Laptiev <artuon@gmail.com>
2021-10-19 21:08:16 +0300

    Custom config establishing

--- custom-config-example/src/app.service.ts
+++ custom-config-example/src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import {ConfigService} from "@nestjs/config";
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
   getHello(): string {
-    return 'Hello World!';
+    const port = this.configService.get<number>('PORT');
+    const commonVar = this.configService.get<number>('COMMON_VAR');
+    return `Hello World! (on port ${port}) <br> COMMON_VAR="${commonVar}"`;
   }
 }
--- custom-config-example/src/main.ts
+++ custom-config-example/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {ConfigService} from "@nestjs/config";
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+  app.listen(port, () => console.log(`The server is running on ${port}`))
 }
 bootstrap();
