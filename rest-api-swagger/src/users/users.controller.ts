03954df20ed8daf6204e5f5facf35db29207333a
Artem Laptiev <artuon@gmail.com>
2021-10-20 21:20:58 +0300

    Users Controller

--- rest-api-swagger/src/users/users.controller.ts
+++ rest-api-swagger/src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { AuthService } from '../auth/auth.service';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
-import { LocalAuthGuard } from '../auth/guards/local-auth.guard';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common'
+
+import { AuthService } from '../auth/auth.service'
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard'
+import { LocalAuthGuard } from '../auth/guards/local-auth.guard'
 
 @Controller('users')
 export class UsersController {
@@ -10,12 +11,12 @@ export class UsersController {
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
-    return this.authService.login(req.user);
+    return this.authService.login(req.user)
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
-    return req.user;
+    return req.user
   }
 }
--- rest-api-swagger/src/users/users.module.ts
+++ rest-api-swagger/src/users/users.module.ts
@@ -1,8 +1,9 @@
-import { forwardRef, Module } from "@nestjs/common";
+import { forwardRef, Module } from '@nestjs/common'
+
+import { AuthModule } from '../auth/auth.module'
 
 import { UsersService } from './users.service'
-import { UsersController } from './users.controller';
-import { AuthModule } from "../auth/auth.module";
+import { UsersController } from './users.controller'
 
 @Module({
   imports: [forwardRef(() => AuthModule)],
