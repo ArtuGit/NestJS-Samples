45fd4034ee9858423c5ce4eeb62b9eb180e8d7a6
Artem Laptiev <artuon@gmail.com>
2021-10-20 21:18:08 +0300

    Users Controller

---
+++ rest-api-swagger/src/users/users.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from '../auth/auth.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { LocalAuthGuard } from '../auth/guards/local-auth.guard';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
--- rest-api-swagger/src/users/users.module.ts
+++ rest-api-swagger/src/users/users.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common'
+import { forwardRef, Module } from "@nestjs/common";
 
 import { UsersService } from './users.service'
+import { UsersController } from './users.controller';
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
+  imports: [forwardRef(() => AuthModule)],
   providers: [UsersService],
   exports: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
