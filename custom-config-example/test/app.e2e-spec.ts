4f2f68d663d22eb1203dd1619e30926e76665b55
Artem Laptiev <artuon@gmail.com>
2021-10-20 12:37:07 +0300

    Fix the import

--- custom-config-example/test/app.e2e-spec.ts
+++ custom-config-example/test/app.e2e-spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common'
 import * as request from 'supertest'
 import { ConfigService } from '@nestjs/config'
 
-import { AppModule } from "../src/app.module"
+import { AppModule } from '../src/app.module'
 
 describe('AppController (e2e)', () => {
   let app: INestApplication
