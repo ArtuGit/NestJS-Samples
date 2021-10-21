3fafff151305ccd1ca2fc551267f08eddc0da04f
Artem L <artuon@gmail.com>
2021-10-21 21:41:01 +0300

    Setup CORS and Swagger

--- rest-api-swagger/src/main.ts
+++ rest-api-swagger/src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core'
 import { ConfigService } from '@nestjs/config'
 import { ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const configService = app.get(ConfigService)
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: '*',
+    methods: '*',
+    exposedHeaders: 'X-Token-Expired',
+  })
+
   // Enable validation
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,7 +24,16 @@ async function bootstrap() {
     }),
   )
 
-  const configService = app.get(ConfigService)
+  // Setup swagger
+  const configSwagger = new DocumentBuilder()
+    .setTitle('NestJS Smaples/Rest API - Swagger')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+  const document = SwaggerModule.createDocument(app, configSwagger)
+  SwaggerModule.setup('api', app, document)
+
   const port = configService.get<number>('PORT')
   app.listen(port, () => console.log(`The server is running on ${port}`))
 }
