b93c7a2946a9745dd8da004241ae94e73d0e3499
Artem Laptiev <artuon@gmail.com>
2021-10-26 22:22:07 +0300

    Better Auth: Tokens Service

---
+++ rest-api-swagger/src/auth/tokens.service.ts
@@ -0,0 +1,111 @@
+import { UnprocessableEntityException, Injectable } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { SignOptions, TokenExpiredError } from 'jsonwebtoken'
+
+import { User } from '../users/entities/user'
+import { UsersService } from '../users/users.service'
+
+import { RefreshTokensRepository } from './refresh-tokens.repository'
+import { RefreshToken } from './entities/refresh-token.entity'
+
+const BASE_OPTIONS: SignOptions = {
+  issuer: 'https://my-app.com',
+  audience: 'https://my-app.com',
+}
+
+export interface RefreshTokenPayload {
+  jti: string
+  sub: string
+}
+
+@Injectable()
+export class TokensService {
+  public constructor(
+    private readonly refreshTokensRepository: RefreshTokensRepository,
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  public async generateAccessToken(user: User): Promise<string> {
+    const opts: SignOptions = {
+      ...BASE_OPTIONS,
+      subject: String(user.id),
+    }
+
+    return this.jwtService.signAsync({}, opts)
+  }
+
+  public async generateRefreshToken(user: User, expiresIn: number): Promise<string> {
+    const token = await this.refreshTokensRepository.createRefreshToken(user, expiresIn)
+
+    const opts: SignOptions = {
+      ...BASE_OPTIONS,
+      expiresIn,
+      subject: String(user.id),
+      jwtid: String(token.id),
+    }
+
+    return this.jwtService.signAsync({}, opts)
+  }
+
+  public async resolveRefreshToken(encoded: string): Promise<{ user: User; token: RefreshToken }> {
+    const payload = await this.decodeRefreshToken(encoded)
+    const token = await this.getStoredTokenFromRefreshTokenPayload(payload)
+
+    if (!token) {
+      throw new UnprocessableEntityException('Refresh token not found')
+    }
+
+    if (token.isRevoked) {
+      throw new UnprocessableEntityException('Refresh token revoked')
+    }
+
+    const user = await this.getUserFromRefreshTokenPayload(payload)
+
+    if (!user) {
+      throw new UnprocessableEntityException('Refresh token malformed')
+    }
+
+    return { user, token }
+  }
+
+  public async createAccessTokenFromRefreshToken(refresh: string): Promise<{ token: string; user: User }> {
+    const { user } = await this.resolveRefreshToken(refresh)
+
+    const token = await this.generateAccessToken(user)
+
+    return { user, token }
+  }
+
+  private async decodeRefreshToken(token: string): Promise<RefreshTokenPayload> {
+    try {
+      return this.jwtService.verifyAsync(token)
+    } catch (e) {
+      if (e instanceof TokenExpiredError) {
+        throw new UnprocessableEntityException('Refresh token expired')
+      } else {
+        throw new UnprocessableEntityException('Refresh token malformed')
+      }
+    }
+  }
+
+  private async getUserFromRefreshTokenPayload(payload: RefreshTokenPayload): Promise<User> {
+    const subId = payload.sub
+
+    if (!subId) {
+      throw new UnprocessableEntityException('Refresh token malformed')
+    }
+
+    return this.usersService.findOneById(subId)
+  }
+
+  private async getStoredTokenFromRefreshTokenPayload(payload: RefreshTokenPayload): Promise<RefreshToken | null> {
+    const tokenId = payload.jti
+
+    if (!tokenId) {
+      throw new UnprocessableEntityException('Refresh token malformed')
+    }
+
+    return this.refreshTokensRepository.findTokenById(tokenId)
+  }
+}
