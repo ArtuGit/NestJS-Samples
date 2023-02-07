07dd16608e0bd4fa7ee64e0e1b81887bfed89ad2
Artu <artuon@gmail.com>
2023-02-07 21:58:46 +0200

    Adding validation

--- nest9-fastify-mongoose/src/main.ts
+++ nest9-fastify-mongoose/src/main.ts
@@ -4,12 +4,23 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  // Enable validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
