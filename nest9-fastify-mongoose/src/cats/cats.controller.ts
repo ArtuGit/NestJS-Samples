8e21715fe81a3891743c2be6ef9c9ce668dc2cf7
Artu <artuon@gmail.com>
2023-02-07 22:25:15 +0200

    Other methods

--- nest9-fastify-mongoose/src/cats/cats.controller.ts
+++ nest9-fastify-mongoose/src/cats/cats.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Body, Param } from '@nestjs/common';
 import { CatsService } from './cats.service';
-import { Cat } from './cat.schema'
+import { Cat } from './cat.schema';
 import { CreateCatBody } from './dto/create-cat.body';
 
 @Controller('cats')
@@ -8,6 +8,21 @@ export class CatsController {
   constructor(private readonly catsService: CatsService) {}
   @Post()
   create(@Body() createCatBody: CreateCatBody): Promise<Cat> {
-      return this.catsService.create(createCatBody)
+    return this.catsService.create(createCatBody);
+  }
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Cat> {
+    return this.catsService.findOne(id);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.catsService.delete(id);
   }
 }
--- nest9-fastify-mongoose/src/cats/cats.service.ts
+++ nest9-fastify-mongoose/src/cats/cats.service.ts
@@ -4,7 +4,6 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Cat, CatDocument } from './cat.schema';
 import { CreateCatBody } from './dto/create-cat.body';
 
-
 @Injectable()
 export class CatsService {
   constructor(@InjectModel(Cat.name) private catModel: Model<CatDocument>) {}
@@ -13,8 +12,18 @@ export class CatsService {
     const createdCat = new this.catModel(createCatBody);
     return createdCat.save();
   }
-
   async findAll(): Promise<Cat[]> {
     return this.catModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<Cat> {
+    return this.catModel.findOne({ _id: id }).exec();
+  }
+
+  async delete(id: string) {
+    const deletedCat = await this.catModel
+      .findByIdAndRemove({ _id: id })
+      .exec();
+    return deletedCat;
   }
 }
